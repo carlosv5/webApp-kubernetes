23f551b69a307ff551588a802a30e2177b8fad7c
evegcar <carlos.vega.garcia@ericsson.com>
2020-03-21 20:23:12 +0100

    Add volumes

--- delete.sh
+++ delete.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+kubectl delete pvc mysql-pvc
+kubectl delete pv 10g-volume.yaml
 kubectl delete services db webapp
 kubectl delete deployments db webapp
--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+kubectl create -f 10g-volume.yaml
+kubectl create -f mysql-pvc.yaml
 kubectl create -f webapp.yaml
 minikube -p master service webapp
