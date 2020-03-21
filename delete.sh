26165af0bdf9d3ff9ace564393d3439c6bdd2768
evegcar <carlos.vega.garcia@ericsson.com>
2020-03-21 20:33:43 +0100

    Fix Port

--- delete.sh
+++ delete.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl delete pvc mysql-pvc
-kubectl delete pv 10g-volume.yaml
-kubectl delete services db webapp
 kubectl delete deployments db webapp
+kubectl delete services db webapp
+kubectl delete pvc mysql-pvc
+kubectl delete pv 10g-volume
