e2c5d1204c42ade3483c6244ac7aa8f4bec5ca7a
evegcar <carlos.vega.garcia@ericsson.com>
2020-03-21 20:41:29 +0100

    Add ingress

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo $MINIKUBE_IP www.carlosvegagarci.com | sudo tee --append /etc/hosts > /dev/null
 kubectl create -f 10g-volume.yaml
 kubectl create -f mysql-pvc.yaml
 kubectl create -f webapp.yaml
